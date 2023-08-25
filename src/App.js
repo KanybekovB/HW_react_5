c5f60dfd22ec616ddf0df3e8722976283310476e
Bektur Kanybekov <kanybekovbektur2007@gmail.com>
2023-08-25 20:56:18 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function PrototypeForm() {
+  const [person, setPerson] = useState({
+    name: "Barbara",
+    surname: "Hepworth",
+    email: "bhepworth@react.com",
+    favoritePet: "love cats",
+  });
+  const handleChange = (e, data) => {
+    setPerson({
+      ...person,
+      [data]: e.target.value,
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <form action="">
+          <div className="input-box">
+            <label>
+              <input
+                type="text"
+                placeholder="Username"
+                value={person.name}
+                onChange={(e) => handleChange(e, "name")}
+                required
+              />
+              {person.name && <p>Name: {person.name}</p>}
+            </label>
+          </div>
+          <div className="input-box">
+            <label>
+              <input
+                type="text"
+                placeholder="Password"
+                required
+                value={person.surname}
+                onChange={(e) => handleChange(e, "surname")}
+              />
+              <p>
+                {person.surname}
+                {""}
+              </p>
+            </label>
+          </div>
+          <div className="input-box">
+            <label>
+              <input
+                type="email"
+                placeholder="Password"
+                required
+                value={person.email}
+                onChange={(e) => handleChange(e, "email")}
+              />
+              <p>{person.email}</p>
+            </label>
+          </div>
+          <div className="input-box">
+            <label>
+              <input
+                type="text"
+                placeholder="Password"
+                required
+                value={person.favoritePet}
+                onChange={(e) => handleChange(e, "favoritePet")}
+              />
+              <p>
+                {person.favoritePet}
+                {""}
+              </p>
+            </label>
+          </div>
+          <button type="submit" className="btn">
+            Click
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
-
-export default App;
